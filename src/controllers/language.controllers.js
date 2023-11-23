23e2a38327abba40734c94feb386b9853a4440b6
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-11-23 02:23:03 -0500

    Update language.controllers.js

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -1,85 +1,196 @@
-import React, { useState, useEffect } from 'react';
-import { getAllOrders, getProductName } from '../api/api';
-interface Sale {
-    id: number;
-    productName: string;
-    name: string;
-    direccion: string;
-    telefono: string;
-    price: number;
-    date: string;
-}
+import { getConnection } from '../database/database';  
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-const Sales: React.FC = () => {
-    const [sales, setSales] = useState<Sale[]>([]);
+const saltRounds = 10;
 
-    console.log(sales)
-    useEffect(() => {
-        getAllOrders()
-            .then(async (orders) => {
-                const salesData: Sale[] = await Promise.all(
-                    orders.map(async (order: any) => {
-                        return {
-                            id: `${order.id_order}-${order.id_product}`,
-                            productName: order.product_name,
-                            name: order.customer_name,
-                            direccion: order.customer_address,
-                            telefono: order.customer_phone,
-                            price: order.price_unit,
-                            date: order.date_order,
-                        };
-                    })
-                );
-                setSales(salesData);
-            })
-            .catch((error) => {
-                console.error('Error al obtener órdenes:', error);
-            });
-    }, []);
-
-    console.log(sales)
-    return (
-        <div className="container mx-auto px-4 py-10">
-            <h1 className="text-2xl font-semibold mb-6">Pedidos</h1>
-
-            <div className="bg-white p-6 rounded-lg shadow-md">
-                {sales.length === 0 ? (
-                    <p className="text-gray-500">No se han registrado ventas aún.</p>
-                ) : (
-                    <table className="min-w-full bg-white border-collapse border border-gray-300">
-                        <thead>
-                            <tr>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Clientes</th>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Producto</th>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Direccion</th>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Telefono</th>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Precio (S/.)</th>
-                                <th className="py-2 px-4 border-b border-gray-300 text-left text-sm font-semibold text-gray-700">Fecha</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sales.map(sale => (
-                                <tr key={sale.id}>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">{sale.name}</td>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">{sale.productName}</td>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">{sale.direccion}</td>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">{sale.telefono}</td>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">{sale.price}</td>
-                                    <td className="py-2 px-4 border-b border-gray-300 text-sm text-gray-500">
-                                        {new Date(sale.date).toLocaleDateString('es-ES', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: 'numeric'
-                                        })}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-            </div>
-        </div>
-    );
+const getUsers = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        connection.query("SELECT username, email, rol, idrol FROM `users`", (error, results) => {
+            connection.release();  
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ message: "Internal Server Error" });
+            }
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
-export default Sales;
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
+
+    if (!id || !username || !password) {
+        return res.status(400).json({ message: "Bad request. Please fill all fields." });
+    }
+
+    const usuario = { username, password };
+
+    try {
+        const connection = await getConnection();
+        connection.query("UPDATE `users` SET ? WHERE id_usuario = ?", [usuario, id], (error, results) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+const getUser = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ message: "Bad request. Please provide a valid user ID." });
+    }
+
+    try {
+        const connection = await getConnection();
+        connection.query("SELECT id_usuario, username FROM `users` WHERE id_usuario = ?", id, (error, results) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ message: "Bad request. Please provide a valid user ID." });
+    }
+
+    try {
+        const connection = await getConnection();
+        connection.query("DELETE FROM `users` WHERE id_usuario = ?", id, (error, results) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+const addUsers = async (req, res) => {
+    const { username, password, email, rol, idrol, direccion, telefono } = req.body;
+
+    if (!username || !password || !email || !rol || !idrol || !direccion || !telefono) {
+        return res.status(400).json({ message: "Please fill all fields." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const usuario = { username, password: hashedPassword, email, rol, idrol, direccion, telefono};
+
+    try {
+        const connection = await getConnection();
+        connection.query("INSERT INTO users SET ?", usuario, (error, results) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log("Attempting to log in with email:", email);
+
+        console.log(password);
+
+        const connection = await getConnection();
+        connection.query("SELECT * FROM `users` WHERE email = ?", email, async (error, users) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+
+            if (users.length > 0) {
+                const user = users[0];
+                const match = await bcrypt.compare(password, user.password);
+
+                if (match) {
+                    const token = jwt.sign({ id: user.id }, "your-secret-key", {
+                        expiresIn: "1h",
+                    });
+                    res.json({ message: "Login successful", token: token, userData: user });
+                } else {
+                    console.log("Password mismatch");
+                    res.status(400).json({ message: "Invalid password" });
+                }
+            } else {
+                console.log("User not found in database");
+                res.status(400).json({ message: "User not found" });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
+};
+
+const getProfile = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ message: "Bad request. Please provide a valid user ID." });
+    }
+
+    try {
+        const connection = await getConnection();
+        connection.query("SELECT id_usuario, username FROM `users` WHERE id_usuario = ?", id, (error, results) => {
+            connection.release();
+            if (error) {
+                console.error(error);
+                return res.status(500).send(error.message);
+            }
+
+            if (results && results.length > 0) {
+                res.json(results[0]);
+            } else {
+                res.status(404).json({ message: "User not found" });
+            }
+        });
+    } catch (error) {
+        console.error("Error al obtener conexión:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const methods = {
+    getUsers,
+    addUsers,
+    getUser,
+    deleteUser,
+    updateUser,
+    loginUser,
+    getProfile,
+};
